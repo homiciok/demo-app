0b5f77dea1d2af124422eefda415949fa0e9f6d6
homiciok <miron.vale@gmail.com>
2016-07-26 12:31:11 +0300

    Update 0.0.2

--- app.js
+++ app.js
@@ -17,8 +17,16 @@ app.controller('MainCtrl', [
 		if (!$scope.title || $scope.title === '') {
 			return;
 		}
-		$scope.posts.push({title: $scope.title, upvotes: 0 });
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0 });
 		$scope.title ='';
+		$scope.link ='';
+	};
+
+	$scope.incrementUpvotes =function(post){
+		post.upvotes += 1;
 	};
 
 }]);
